a88690bbc95febada73177dd0035a2bc8e9fca2d
ixphinx <riktmp@gmail.com>
2019-12-05 21:44:00 +0000

    desarrollo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,8 @@
 const express = require('express');
-const router = express('router');
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+    res.render('index.ejs');
+});
 
 module.exports = router;
