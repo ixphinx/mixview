61333d8b0cfe91604785289424792b446b369bf7
ixphinx <48696004+ixphinx@users.noreply.github.com>
2021-07-19 22:51:00 -0300

    update data

--- src/models/database.js
+++ src/models/database.js
@@ -4,17 +4,9 @@ const {Schema} = mongoose;
 const Data = new Schema({
     
     /* Data */
-    name: String,
-    lastname: String,
-    company: String,
-    mail: String,
-    phone: String,
-    country: String,
-    city: String,
-    video: String,
-    news: String,
-    newsid: String,
-    newsbody: String,
+    class: String,
+    videoid: String,
+    videourl: String,
 
 
 });
